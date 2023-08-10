0abad93689779954d7b7dfa5e2afc69396396d10
Jason <jason.matillano@gmail.com>
2023-08-11 00:08:52 +0800

    flexDirection: 'row',         justifyContent: 'space-between',

--- components/sandbox.js
+++ components/sandbox.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     container: {
         // flex: 1,
         flexDirection: 'row',
-        justifyContent: 'space-around',
+        justifyContent: 'space-between',
         // alignItems: 'center',
         padding: 10,
         backgroundColor: '#ddd',
