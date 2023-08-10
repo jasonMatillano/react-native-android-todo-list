cad61aaeefd6929ce0e385e12f1cc5a42f7dcb73
Jason <jason.matillano@gmail.com>
2023-08-10 15:20:47 +0800

    0.1.2.AddedAlertPromtForInvalidInput

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList} from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert} from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
@@ -19,13 +19,24 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
+
+    if(text.length > 3) {
+      setTodos((prevTodos) => {
         return [
           { text: text, key: Math.random().toString() }, // generate random id
           ...prevTodos // include previous array
         ]
-      }
-    )
+      })
+    } else {
+      Alert.alert(
+        'Error',
+        'Please enter a longer text',
+        [
+          { text: 'OK', onPress: () => console.log('OK Pressed') },
+        ],
+        { cancelable: false }
+      );
+    }
   }
 
   return (
