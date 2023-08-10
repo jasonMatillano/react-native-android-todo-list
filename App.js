82a6a3d10698f891ac8808f7c9f0af3059f5f899
Jason <jason.matillano@gmail.com>
2023-08-10 14:39:13 +0800

    0.1.1.AddTodoFeatureUsingProps

--- App.js
+++ App.js
@@ -2,30 +2,42 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList} from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
-    { text: 'buy coffee', key: '1' },
+    { text: 'buy coffees', key: '1' },
     { text: 'create an app', key: '2' },
     { text: 'play on the switch', key: '3' }
   ]);
 
   const pressHandler = (key) => {
     setTodos((prevTodos) => {
+      console.log(key)
       return prevTodos.filter(todo => todo.key != key);
     }); 
+  }
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() }, // generate random id
+          ...prevTodos // include previous array
+        ]
+      }
+    )
   }
 
   return (
     <View style={styles.container}>
       <Header/>
       <View style={styles.content}> 
-       {/* to form */}
+       <AddTodo submitHandler={submitHandler} /* send prop submitHandler *//> 
        <View style={styles.list}>
          <FlatList
           data={todos}
           renderItem={({item}) => (
-          <TodoItem item={item} pressHandler={pressHandler}/>
+          <TodoItem item={item} pressHandler={pressHandler} /* send prop item & pressHandler*/  />
           )}
          />
        </View>
---
+++ components/addTodo.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+
+export default function AddTodo( {submitHandler} ) { //receive prop func
+
+    const [text, setText] = useState('');
+
+    const changeHandler = (text) => {
+        setText(text);
+    }
+
+    return (
+        <View>
+          <TextInput
+            style={styles.input}
+            placeholder='new todo...'
+            onChangeText={changeHandler}// track the change and value of TextInput
+          ></TextInput>
+          <Button
+            onPress={() => submitHandler(text)}
+            title='add todo'
+            color='coral'
+          ></Button>
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    input: {
+      marginBottom: 10,
+      paddingHorizontal: 8,
+      paddingVertical: 6,
+      borderBottomWidth: 1,
+      borderBottomColor: '#ddd',
+    },
+});
+    
